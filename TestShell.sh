49a6091ffc36e9ec6c74924f8a3f5d20b7bef906
Mahjabin Alam <natashamalam@users.noreply.github.com>
2023-03-10 16:14:03 +0900

    Update TestShell.sh

--- TestShell.sh
+++ TestShell.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-xcodebuild clean
-
 xcodebuild -project KeyPad.xcodeproj -scheme KeyPad -destination 'platform=iOS Simulator,name=iPhone 14'
-
-xcodebuild -project KeyPad.xcodeproj -scheme KeyPad -archivePath ${pwd}/KeyPad.xcarchive -destination generic/platform=iOS archive
